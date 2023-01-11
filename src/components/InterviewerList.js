72e90fbf29f7316ae6b0dd63b870b75a4652d26d
Ryan De Mesa <ryandemesa09@gmail.com>
2023-01-11 16:26:48 -0700

    Refactor prop names in components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,15 +3,14 @@ import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  
   const interviewerList = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
