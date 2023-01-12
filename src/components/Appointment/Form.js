417b18a1385ed1b6d84d5c9d5e58a095bba7b367
Ryan De Mesa <ryandemesa09@gmail.com>
2023-01-11 21:44:50 -0700

    Refactor Form to use props & add button functionality

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,17 +3,34 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
-
+  
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = function() {
+    props.onCancel();
+    reset();
+  };
+
+  const save = function() {
+    props.onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form 
+          autoComplete="off"
+          onSubmit={event => event.preventDefault()}
+        >
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={props.name}
             type="text"
             placeholder="Enter Student Name"
             value={student} 
@@ -26,10 +43,11 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
   );
+  
 }
