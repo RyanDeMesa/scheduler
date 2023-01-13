25816e1844ccceac6814c622b6a5c675d7173ade
Ryan De Mesa <ryandemesa09@gmail.com>
2023-01-12 22:06:48 -0700

    Add helps folder & getAppointmentsForDay funciton/test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+export function getAppointmentsForDay(state, day) {
+
+  // returns object matching day
+  const matchingDay = state.days.find((days) => days.name === day);
+
+  // if day is not found, return empty object
+  if (!matchingDay) return [];
+
+  // returns array of appointment ids for day
+  const appointmentIds = matchingDay.appointments;
+
+  // returns a new array where each appointmentId is replaced with it's corresponding appointment details
+  const appointmentsList = appointmentIds.map((id) => state.appointments[id]);
+  return appointmentsList;
+}
